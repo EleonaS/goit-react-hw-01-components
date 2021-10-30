618606677ef3dd1d7e49f4668d4df9013f99bb52
Elena Sysoyeva <evsysoyeva@gmail.com>
2021-10-30 20:42:29 +0300

    task1, task2

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+
+import user from './data/user.json';
+import Profile from './components/Profile';
+
+import statisticalData from './data/statistical-data.json';
+import Statistics from './components/Statistic/Statistics';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img
-          src={logo}
-          className="App-logo"
-          alt="logo"
+      <div>
+        <Profile
+          name={user.name}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
         />
-        <p>
-          Edit <code>src/App.js</code>{' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div>
+        <Statistics
+          stats={statisticalData}
+        />
+      </div>
     </div>
   );
 }
